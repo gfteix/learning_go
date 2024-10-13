1dd1fec74737719088a1ef061332f09009b7cea5
gfteix <gabriel.fteixeira13@gmail.com>
2024-10-13 15:34:50 -0300

    building array method such as map and filter

---
+++ array-methods/main.go
@@ -0,0 +1,89 @@
+package main
+
+import "fmt"
+
+func Filter[T any](slice []T, fn func(T) bool) []T {
+	var result []T
+
+	for _, v := range slice {
+		response := fn(v)
+
+		if response {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
+
+func Map[T any](slice []T, fn func(T) T) []T {
+	var result []T
+
+	for _, v := range slice {
+		result = append(result, fn(v))
+	}
+
+	return result
+}
+
+func ForEach[T any](slice []T, fn func(T)) {
+	for _, v := range slice {
+		fn(v)
+	}
+}
+
+func Some[T any](slice []T, fn func(T) bool) bool {
+	for _, v := range slice {
+		if fn(v) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func Every[T any](slice []T, fn func(T) bool) bool {
+	for _, v := range slice {
+		if !fn(v) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func Contains[T comparable](slice []T, e T) bool {
+	for _, v := range slice {
+		if v == e {
+			return true
+		}
+	}
+
+	return false
+}
+
+// TODO: REDUCE
+
+func main() {
+	s := []int{1, 2, 3, 4, 6}
+	fmt.Printf("\nINITIAL %v", s)
+
+	// Filter
+	fn := func(n int) bool {
+		return n%2 == 0
+	}
+
+	filtered := Filter(s, fn)
+
+	fmt.Printf("\nFILTER %v", filtered)
+
+	// Map
+
+	f := func(n int) int {
+		return n * 2
+	}
+
+	updated := Map(s, f)
+
+	fmt.Printf("\nMAP %v", updated)
+}
